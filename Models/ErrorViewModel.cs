eb86c7994a6af2bcaa8aae3a2d8696f50b995c21
Anjali-st <anjali90908080@gmail.com>
2023-09-11 08:25:50 +0530

    Ajax-Enabled Forms

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sec_04Ajax_EnabledForms.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Threading.Tasks;
+namespace Sec_04Ajax_EnabledForms.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Class { get; set; }
+
+        public string Email { get; set; }
+
+    }
+
+} 
+  
